a32bb146b9c04c28df46f321654251ca21d53f4c
Danylo Dias Gomes <danylodiasgomes@hotmail.com>
2020-02-16 11:53:59 -0300

    aula 01 pw

---
+++ PW II/Aula 01/Exercícios/Aula1-Exer1/index.php
@@ -0,0 +1,5 @@
+<?php
+    $a = 5;
+    $b = $a * $a;
+    print("1) O quadrado de ".$a." = ".$b);
+?>
---
+++ PW II/Aula 01/Exercícios/Aula1-Exer3/index.php
@@ -0,0 +1,8 @@
+<?php
+    $x = 8;
+    if ($x % 2 == 0){
+        print("3) É Par");
+    }else{
+        print("3) É impar");
+    }
+?>
